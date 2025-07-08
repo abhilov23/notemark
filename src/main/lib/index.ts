5021b4c82440d4ba0d413fad2938e9c881b747df
Abhilov23 <abhilovgupta01@gmail.com>
2025-07-08 15:32:39 +0530

    check this

---
+++ src/main/lib/index.ts
@@ -0,0 +1,37 @@
+/* eslint-disable prettier/prettier */
+/* eslint-disable @typescript-eslint/explicit-function-return-type */
+import { readdir } from "fs"
+import { ensureDir } from "fs-extra"
+import { homedir } from "os"
+import { appDirectoryName, fileEncoding } from "src/shared/constants"
+import { NoteInfo } from "src/shared/models"
+
+export const getRootDir = ()=>{
+    return `${homedir()}/${appDirectoryName}`
+}
+
+export const getNotes = async ()=>{
+    const rootDir = getRootDir()
+
+    await ensureDir(rootDir)
+
+    const notesFileNames = await readdir(rootDir, {
+        encoding: fileEncoding,
+        withFileTypes: false
+    })
+   
+    const notes = notesFileNames.filter((fileName) => fileName.endsWith('.md'))
+
+
+    return Promise.all(notes.map(getNotesInfoFromFileName)) 
+}
+
+export const getNotesInfoFromFileName = async (fileName: string): Promise<NoteInfo> => {
+   
+    const fileStats = await stat(`${getRootDir()}/${fileName}`)
+
+    return {
+        title: fileName.replace('.md', ''),
+        lastEditTime: fileStats.mtime.getTime()
+    }
+}
